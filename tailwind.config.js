1602c379d0e3e872363f0efa31253f62f61b647b
Surya Veer Singh <suryaveer046@gmail.com>
2026-01-27 14:58:01 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
           600: '#7c3aed',  // Primary color
           700: '#6d28d9',
           800: '#5b21b6',
-          900: '#4c1d95',
+          900: '#4c1d95', 
           950: '#2e1065',
         },
         secondary: {
